30abac530b6c8ebaaf2abf0ed894c9de6a0b8318
Aakash Raj <aakash.raj5@gmail.com>
2022-04-06 19:53:56 -0500

    Development: Iteration 1 (#10)
    * Changes to setup react with rails
    
    * DB + Login Controller
    
    * Added Active Record
    
    * Added changes to include react
    
    * Gemfile changes
    
    * Webpacker changes
    
    * Gemfile Changes
    
    * Heroku + Docker Setup
    
    * Added UI changes for Login, Sign Up and added validation
    
    * Updated Readme
    
    * Added documentation for iteration 1
    
    * Feature/login (#1)
    
    * Changes to setup react with rails
    
    * DB + Login Controller
    
    * Added Active Record
    
    * Added changes to include react
    
    * Gemfile changes
    
    * Webpacker changes
    
    * Gemfile Changes
    
    * Heroku + Docker Setup
    
    * Added UI changes for Login, Sign Up and added validation
    
    * Updated Readme
    
    * Added documentation for iteration 1
    
    Co-authored-by: Vibush <vibushks@tamu.edu>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-87-204.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-91-118.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-81-44.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-83-168.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-90-192.ec2.internal>
    
    * Added form builder component now
    
    * Changes for persona homepages and css changes
    
    * MongoDB Integration
    
    * Added changes for user event sign up
    
    * Pushed Admin + Client Controllers
    
    * Added changes for user event registration
    
    * Pushed Logout and Session features
    
    * Added css changes to admin homepage
    
    * Updated the UI
    
    * sync up
    
    * added models for mongodb
    
    * resolved user events conflict
    
    * added documentation for iter 2
    
    Co-authored-by: Vibush <vibushks@tamu.edu>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-87-204.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-91-118.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-81-44.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-83-168.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-90-192.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-93-168.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-83-81.ec2.internal>
    Co-authored-by: varunlpai-tamu <varunlpai@tamu.edu>
    Co-authored-by: varunlpai-tamu <99226437+varunlpai-tamu@users.noreply.github.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-54-152.ec2.internal>
    Co-authored-by: Vibush Shanmugam <99617026+vibushks@users.noreply.github.com>

--- web/CASTNXT/app/javascript/components/Admin/AdminHomepage.js
+++ web/CASTNXT/app/javascript/components/Admin/AdminHomepage.js
@@ -9,21 +9,44 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
+import { Redirect } from 'react-router-dom';
+import './Admin.css';
 
 class AdminHomepage extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            redirect: "",
         }
+    }
+    
+    createEventRedirection = () => {
+        this.setState({
+            redirect: 'admin/create-event'
+        })
     }
 
     render() {
+        if(this.state.redirect === "admin/create-event") {
+            return <Redirect to='/admin/create-event'/>;
+        }
+        
+        
         let borderstyle = {
             border: '1px solid black',
             borderTopRightRadius: '5px',
             borderTopLeftRadius: '5px'
+        }
+        
+        let adminHomepageStyle = {
+            marginTop: '3%',
+            backgroundColor: 'white',
+            height: '100vh',
+            paddingTop: '1%',
+            paddingBottom: '5%',
+            overflowY: 'scroll'
         }
         
         return(
@@ -32,11 +55,11 @@ class AdminHomepage extends Component {
                     <Header />
                 </div>
                 
-                <div style={{marginTop: '3%', backgroundColor: 'white'}}>
+                <div style={adminHomepageStyle}>
                     <h1>Admin Homepage</h1>
                     
 
-                    <div className="container" style={{ backgroundColor: 'gray', height: '80vh', width: '50vw', paddingTop: '1%' }}>
+                    <div className="container" style={{ backgroundColor: 'white', height: '100%', width: '50vw', paddingTop: '1%' }}>
                         
                         <div style={{ color: 'white' }}>
                             <Tabs variant="fullWidth" value={this.state.tabValue} onChange={this.handleTabChange} centered>
@@ -51,6 +74,9 @@ class AdminHomepage extends Component {
     
                         <br />
                         <p>Use this page to sign up for events and check to see if you've been selected for an event</p>
+                        <div style={{margin: 20}}>
+                            <Button variant="contained" onClick={this.createEventRedirection}>Create New Form</Button>
+                        </div>
                         <div className="row">
                             <div className="col-md-10 offset-md-1">
                                 <TableContainer component={Paper}>
